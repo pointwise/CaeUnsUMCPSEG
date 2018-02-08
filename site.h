e44ddc2f47b83eb2f29132d9437f867e829b80a4
David Garlisch <david.garlisch@gmail.com>
2018-02-08 16:56:46 -0600

    Add files via upload
    plugin specific site.h
    Forces default group id
    Changes group name to "GITHUB"

---
+++ site.h
@@ -0,0 +1,41 @@
+/****************************************************************************
+*
+* Copyright (c) 2012-2018 Pointwise, Inc.
+* All rights reserved.
+*
+* This sample Pointwise plugin is not supported by Pointwise, Inc.
+* It is provided freely for demonstration purposes only.
+* SEE THE WARRANTY DISCLAIMER AT THE BOTTOM OF THIS FILE.
+*
+***************************************************************************/
+
+#ifndef _GITHUBSITE_H_
+#define _GITHUBSITE_H_
+
+#if !defined(BUILD_PWPLUGIN_DYNLIB)
+#   define BUILD_PWPLUGIN_DYNLIB
+#endif
+
+#undef PWP_SITE_GROUPID
+
+#undef PWP_SITE_GROUPNAME
+#define PWP_SITE_GROUPNAME   "GITHUB"
+
+#endif
+
+/****************************************************************************
+*
+* DISCLAIMER:
+* TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, POINTWISE DISCLAIMS
+* ALL WARRANTIES, EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED
+* TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+* PURPOSE, WITH REGARD TO THIS SCRIPT. TO THE MAXIMUM EXTENT PERMITTED
+* BY APPLICABLE LAW, IN NO EVENT SHALL POINTWISE BE LIABLE TO ANY PARTY
+* FOR ANY SPECIAL, INCIDENTAL, INDIRECT, OR CONSEQUENTIAL DAMAGES
+* WHATSOEVER (INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS OF
+* BUSINESS INFORMATION, OR ANY OTHER PECUNIARY LOSS) ARISING OUT OF THE
+* USE OF OR INABILITY TO USE THIS SCRIPT EVEN IF POINTWISE HAS BEEN
+* ADVISED OF THE POSSIBILITY OF SUCH DAMAGES AND REGARDLESS OF THE
+* FAULT OR NEGLIGENCE OF POINTWISE.
+*
+***************************************************************************/
